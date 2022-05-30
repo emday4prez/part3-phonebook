f487f21996a0d22e3e226a74e2945772776cb5a1
Emerson <emerson.day@gmail.com>
2022-05-30 14:55:33 -0500

    added build folder

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 morgan.token('person', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
 
   let persons = [
     { 
